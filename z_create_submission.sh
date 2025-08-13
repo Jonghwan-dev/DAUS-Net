2cb0b6f8b22b465550c4d689e89615d0b315edb1
Jonghwan-dev <army@ncc.re.kr>
2025-08-14 04:30:35 +0900

    : add submission bash file

--- z_create_submission.sh
+++ z_create_submission.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export CKPT="train_output/run_250813_042520/best_model_tm_147_0.8093.pth"
 export FILM_SCALE=0.7
 export PRIOR_LAMBDA=0.5
-export OUTPUT_DIR="submission_result/results"
+export OUTPUT_DIR="submission_result/final"
 export CLS_HEAD_VARIANT="linear"
 export CLS_DROPOUT=0.0
 
